872f3030d0a59e392a0d1327ef42a417a156d5b2
Alumno <Alumno@LAB1504-18>
2019-10-21 16:00:08 -0500

    commit first

---
+++ Lab10/Lab10/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using Lab10.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+         
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Operaciones());
+
+            };
+         
+            
+
+            
+        }
+    }
+}
---
+++ Lab10/Lab10/Views/SimpleDemo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab10.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SimpleDemo : ContentPage
+	{
+		public SimpleDemo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
